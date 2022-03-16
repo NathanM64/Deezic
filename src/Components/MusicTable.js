f0e52186144cf9906726c048efbd5cde00d9e85b
Nathan Marimbordes <marimbordes.nathan@gmail.com>
2022-03-16 22:17:44 +0100

    feat: add rating component in table

--- src/Components/MusicTable.js
+++ src/Components/MusicTable.js
@@ -1,10 +1,11 @@
 import { TableBody, Table, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import TrackDetails from './TrackDetails';
 import { Link } from 'react-router-dom';
+import RatingTable from './RatingTable';
 
 function MusicTable({ data }) {
   return (
-    <TableContainer sx={{ color: 'white' }}>
+    <TableContainer sx={{ color: 'white', mb: 20 }}>
       <Table>
         <TableHead>
           <TableRow>
@@ -20,7 +21,7 @@ function MusicTable({ data }) {
               <TableCell component="td" scope="row"><Link to={`/music/${track.track.commontrack_id}`} element={<TrackDetails />} className="nav-link table">{track.track.track_name}</Link></TableCell>
               <TableCell component="td" scope="row"><Link to={`/artist/${track.track.artist_id}`} element={<TrackDetails />} className="nav-link table">{track.track.artist_name}</Link></TableCell>
               <TableCell component="td" scope="row"><Link to={`/album/${track.track.album_id}`} element={<TrackDetails />} className="nav-link table">{track.track.album_name}</Link></TableCell>
-              <TableCell component="td" scope="row">{track.track.track_rating}</TableCell>
+              <TableCell component="td" scope="row"> <RatingTable value={track.track.track_rating} /></TableCell>
             </TableRow>
           ))}
         </TableBody>
---
+++ src/Components/RatingTable.js
@@ -0,0 +1,31 @@
+import { Tooltip, Typography } from "@mui/material";
+
+function RatingTable({ value }) {
+  const rating = Math.floor(parseInt(value / 10));
+
+  const getRate = () => {
+    let items = []
+    for (let i = 0; i < rating; i++) {
+      items.push(<Typography variant="body2" sx={{ color: 'white' }}>l</Typography>)
+    }
+    for (let i = 0; i < 10 - rating; i++) {
+      items.push(<Typography variant="body2" sx={{ color: 'black' }}>l</Typography>)
+    }
+
+    return items;
+  }
+
+  return (
+    <div >
+      <Tooltip title={`${value / 10}/10`} placement="right">
+        <span style={{ display: 'flex' }}>
+          {
+            getRate()
+          }
+        </span>
+      </Tooltip>
+    </div>
+  )
+}
+
+export default RatingTable;
